d15dd025c0a350aad3ca48098c2ee08568cc05c2
t <t@vbox.trcont.ru>
2025-12-16 22:17:19 +0300

    add k8s && update ci

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,15 @@
-print("Hello world! Don't  sleep! Time to work")
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/health":
+           self.send_response(200)
+           self.end_headers()
+           self.wfile.write(b"OK")
+        else:
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write("Hello world! Don't  sleep! Time to work!")
+
+server = HTTPServer(("0.0.0.0", 9000), SimpleHandler)
+server.serve_forever()
